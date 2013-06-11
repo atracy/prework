67df5189afbfcb9d4a8b5f8f58ab6a30f54f5106
Aaron Tracy <atracy@gmail.com>
2013-06-11 16:11:50 -0600

    initial checkin

---
+++ YourAgeInSeconds.rb
@@ -0,0 +1,7 @@
+years = 39
+daysinayear = 365
+hoursinaday = 24
+minutesinanhour = 60
+secondsinaminute = 60
+
+puts 'I am ' + (years * daysinayear * hoursinaday * minutesinanhour * secondsinaminute).to_s + ' seconds old!' 
