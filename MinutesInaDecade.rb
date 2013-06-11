a88e49d83cf4898fdbc5a40825aca9e38ac42b76
Aaron Tracy <atracy@gmail.com>
2013-06-11 16:07:48 -0600

    Initial Checkin

---
+++ MinutesInaDecade.rb
@@ -0,0 +1,8 @@
+#Minutes in a decade
+
+years = 10
+daysinayear = 365
+hoursinaday = 24
+minutesinanhour = 60
+
+puts "There are " + (years * daysinayear * hoursinaday * minutesinanhour).to_s + " minutes in a decade"
