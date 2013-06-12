00f9e02e361add8ffd8fc553b3161521470a84f0
Aaron Tracy <atracy@gmail.com>
2013-06-12 13:50:23 -0600

    initial checkin

---
+++ 99bottlesofbeer.rb
@@ -0,0 +1,11 @@
+
+numberofbottles = 99
+
+while numberofbottles -1 > -1
+	puts numberofbottles.to_s + ' of beer on the wall'
+	puts numberofbottles.to_s + ' bottles of beer!'
+	puts 'take one down and pass it around'
+	numberofbottles = numberofbottles - 1
+	puts numberofbottles.to_s + ' of beers on the wall!'
+
+end
