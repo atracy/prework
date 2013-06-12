4572388bd398f5a0a10fc9cdb8e35f24eda970eb
Aaron Tracy <atracy@gmail.com>
2013-06-12 09:56:27 -0600

    Initial Checkin

---
+++ FullNameGreeting.rb
@@ -0,0 +1,8 @@
+puts 'Hi!  Can you please enter your first name?'
+firstname = gets.chomp
+puts 'Thank you!  Can you please Enter your Middle Name?'
+middlename = gets.chomp
+puts 'Thanks again!  Finally, can you please enter your last name?'
+lastname = gets.chomp
+
+puts 'Pleased to meet you, ' + firstname + ' ' + middlename + ' ' + lastname + '!!'
